1d253c2fb75f0f2676c0444152ba1f4fad76ac5d
kryacose <kuriakose.el@gmail.com>
2018-10-10 10:21:32 +0800

    utils completed

--- utils/board.dart
+++ utils/board.dart
@@ -6,8 +6,8 @@ class Board{
   int score;
   int size;
 
-  Board(){
-    this.size = 4;
+  Board(size){
+    this.size = size;
     this.score = 0;
     this.grid = new List<List<int>>();
 
@@ -19,19 +19,45 @@ class Board{
       grid.add(list);
     }
 
-    print("\nNew Board created $grid,    score $score,    size $size");
+    print("\nNew Board created,  score $score,    size $size");
 
   }
 
-  void randomNum(){
+  void display(){
+    print("\nScore: $score\nSize: $size");
+    for(var i=0; i<size; i++){
+      print(grid[i]);
+    }
+  }
+
+  int randomNum(){
+    if(zeroCount() <= 0) return 0;
+    
     var rnd = new Random();
     int num = (rnd.nextInt(2) + 1)*2;
     int loc = rnd.nextInt(16);
-    while( this.grid[loc%size][loc~/size] != 0)
+    while( this.grid[loc~/size][loc%size] != 0)
       loc = rnd.nextInt(16);
-    this.grid[loc%size][loc~/size] = num;
+    this.grid[loc~/size][loc%size] = num;
 
     print("\nRandom number $num at location $loc");
+    return 1;
+  }
+
+  int putNum(int number, int loc){
+    if(grid[loc~/size][loc%size] != 0) return 0;
+
+    grid[loc~/size][loc%size] = number;
+    return 1;
+  }
+
+  int zeroCount(){
+    int sum = 0;
+    for(var i=0; i<size; i++)
+      for(var j=0; j<size; j++)
+        if(grid[i][j] == 0) sum++;
+
+    return sum;
   }
 
   List<List<int>> up(){
@@ -62,7 +88,7 @@ class Board{
           newGrid[j][i] = 0;
         }
 
-        else if(newGrid[loc][i] == newGrid[j][i]){
+        else if(newGrid[loc][i] == newGrid[j][i] && loc!=j){
           newGrid[loc][i] *= 2;
           newGrid[j][i] = 0;
         }
@@ -98,7 +124,7 @@ class Board{
           newGrid[j][i] = 0;
         }
 
-        else if(newGrid[loc][i] == newGrid[j][i]){
+        else if(newGrid[loc][i] == newGrid[j][i] && loc!=j){
           newGrid[loc][i] *= 2;
           newGrid[j][i] = 0;
         }
@@ -107,29 +133,115 @@ class Board{
     return newGrid;
     
   }
-}
-//   List<List<int>> left(){
-    
-//   }
 
-//   List<List<int>> right(){
+  List<List<int>> left(){
+    List<List<int>> newGrid = new List<List<int>>();
+
+    for(var i = 0; i < size; i++) {
+      List<int> list = new List<int>();
+      for (var j = 0; j < size; j++)
+        list.add(grid[i][j]);
+      newGrid.add(list);
+    }
+
     
-//   }
-// }
+    for(int i=0; i<size; i++){
+      int done = 0;
+      for(int j=1; j<size; j++){
+        int loc = j;
+        if(newGrid[i][j] == 0) continue;
+        for(int k=j-1; k>=0; k--){
+          if(newGrid[i][k] == 0) loc = k;
+          else if(newGrid[i][k] == newGrid[i][j] && done == 0 ) {loc = k; done = 1; break;}
+          else {done = 0; break;}
+        }
+
+        if(newGrid[i][loc] == 0){
+          newGrid[i][loc] = newGrid[i][j];
+          newGrid[i][j] = 0;
+        }
+
+        else if(newGrid[i][loc] == newGrid[i][j] && loc!=j){
+          newGrid[i][loc] *= 2;
+          newGrid[i][j] = 0;
+        }
+      }
+    }
+    return newGrid;
+    
+  }
+
+  List<List<int>> right(){
+    List<List<int>> newGrid = new List<List<int>>();
+
+    for(var i = 0; i < size; i++) {
+      List<int> list = new List<int>();
+      for (var j = 0; j < size; j++)
+        list.add(grid[i][j]);
+      newGrid.add(list);
+    }
+    
+    for(int i=0; i<size; i++){
+      int done = 0;
+      for(int j=size-2; j>=0; j--){
+        int loc = j;
+        if(newGrid[i][j] == 0) continue;
+        for(int k=j+1; k<size; k++){
+          if(newGrid[i][k] == 0) loc = k;
+          else if(newGrid[i][k] == newGrid[i][j] && done == 0 ) {loc = k; done = 1; break;}
+          else {done = 0; break;}
+        }
+
+        if(newGrid[i][loc] == 0){
+          newGrid[i][loc] = newGrid[i][j];
+          newGrid[i][j] = 0;
+        }
+
+        else if(newGrid[i][loc] == newGrid[i][j] && loc!=j){
+          newGrid[i][loc] *= 2;
+          newGrid[i][j] = 0;
+        }
+      }
+    }
+    return newGrid;
+    
+  }
+}
+
+
 
 
 void main(){
   // print("Hello");
-  Board b = new Board();
+  Board b = new Board(4);
 
-  b.randomNum();
-  b.randomNum();
-  b.randomNum();
-  b.randomNum();
+  b.putNum(2, 0);
+  b.putNum(4, 1);
+  b.putNum(2, 2);
+  b.putNum(2, 3);
+  b.putNum(2, 4);
+  b.putNum(2, 5);
+  b.putNum(4, 6);
+  b.putNum(2, 7);
+  b.putNum(2, 8);
+  b.putNum(2, 9);
+  b.putNum(2, 10);
+  b.putNum(4, 11);
+  b.putNum(2, 12);
+  b.putNum(2, 13);
+  b.putNum(2, 14);
 
-  print(b.grid);
 
-  b.grid = b.up();
-  b.grid = b.down();
-  print(b.grid);
+
+  var x = b.randomNum();
+  print("Randnum: $x");
+  // b.randomNum();
+  // b.randomNum();
+
+  b.display();
+
+  // b.grid = b.left();
+  // b.display();
+  // b.grid = b.left();
+  // b.display();
 }
